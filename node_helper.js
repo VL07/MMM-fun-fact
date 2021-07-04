3ff9528a3af4fd2df056d4f49dda83e3bdc33253
VL07 <79533745+VL07@users.noreply.github.com>
2021-07-04 18:13:02 +0200

    Added stop function

--- node_helper.js
+++ node_helper.js
@@ -1,7 +1,16 @@
 var NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
     
+    requiresVersion: "2.1.0",
+
+    // On start debug
     start: function() {
         Log.log(this.name + ' is started!');
+    },
+
+    // On stop debug
+    stop: function() {
+        console.log("Shutting down " + this.name);
+        this.connection.close();
     }
 });
